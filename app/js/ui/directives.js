887b3c5f74cc6d954176dd9b746fcb56661ce7fb
JimyRyan <JimyRyan@gmail.com>
2015-11-18 23:22:09 +0100

    ok

--- app/js/ui/directives.js
+++ app/js/ui/directives.js
@@ -43,11 +43,69 @@ angular.module('angularMovieUI').directive('editable', function() {
 
 });
 
-angular.module('angularMovieUI').directive('rating', function() {
+angular.module('angularMovieUI').directive('movieRating1', function() {
+
+  return {
+    restrict    : 'E',
+    replace     : true,
+    scope       : {
+      rate : '@'
+    },
+    link        : function(scope, element, attrs, ctrl, transclude) {
+
+
+      var count = 0;
+      do {
+        transclude(function (data) {
+          element.append(data);
+        });
+        count++;
+      } while (parseInt(scope.rate) > count);
+
+    },
+    transclude : true,
+    template: '<div></div>' // Dans le template : pas besoin de placer le scope
+  }
+
+});
+
+
+
+angular.module('angularMovieUI').directive('movieRating2', function() {
+
+  return {
+    restrict    : 'E',
+    transclude : true,
+    scope       : {
+      rate : '=ngModel',  // = : Obligatoire (dans le HTML : ... ng-model="movie.rate" ... (<rating ng-model="movie.rate" max="5"><i class="fa fa-star"></i></rating>)
+      max: '=?'     // =? : Optionel
+    },
+    link        : function(scope, element, attrs) {
+
+      scope.max = scope.max || 5;
+
+      scope.rateArray = [];
+
+      for (var i = 0 ; i < scope.rate ; i++) {
+        scope.rateArray.push({'filled': (i < scope.rate ? true : false)}); //filled => étoile remplie...
+      };
+
+    },
+    template   : // Dans le template : pas besoin de placer le scope
+    '  <div><span ng-repeat="star in rateArray">' +
+    '    <ng-transclude></ng-transclude>' +
+    '  </span></div>'
+
+  }
+
+});
+
+
+angular.module('angularMovieUI').directive('movieRating3', function() {
   return {
     restrict   : 'E',
     transclude : true,
-    template   : '<form><input><input></form><ul class="rating readonly">' +
+    template   : '<ul class="rating readonly">' + // Dans le template : pas besoin de placer le scope
     '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}">' +
     '    <ng-transclude></ng-transclude>' +
     '  </li>' +
@@ -76,7 +134,62 @@ angular.module('angularMovieUI').directive('rating', function() {
   };
 });
 
+angular.module('angularMovieUI').directive('movieRating4', function() {
+  return {
+    restrict   : 'E',
+    transclude : true,
+    template   : '<ul class="rating {readonly: readonly}">' + // Dans le template : pas besoin de placer le scope
+    '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-click="rate($index)">' +
+    '    <ng-transclude></ng-transclude>' +
+    '  </li>' +
+    '</ul>',
+    scope      : {
+      ratingValue : '=ngModel',
+      max         : '=?',
+      readonly    : '=?'
+    },
+    link       : function(scope, element, attributes) {
+      scope.max = scope.max || 5;
+      scope.readonly = scope.readonly || false;
 
+      function updateValue() {
+        scope.stars = [];
+        for (var i = 0; i < scope.max; i++) {
+          scope.stars.push({
+            filled : i < scope.ratingValue
+          });
+        }
+      }
+      scope.rate = function(index) {
+        if (scope.readonly === false) {
+          scope.ratingValue = index + 1;
+        }
+      };
+      scope.$watch('ratingValue', function(newValue) {
+        if (newValue || scope.stars == undefined) {
+          updateValue();
+        }
+      });
+    }
+  };
+});
+
+// Sync validator
+angular.module('angularMovieUI').directive('directorValidator', function() {
+  return {
+    require : 'ngModel',
+    link : function($scope, element, attrs, ngModel) {
+      ngModel.$validators.director = function(value) {
+        if(value) {
+          return value.split(' ').length > 1;
+        }
+      };
+    }
+  }
+});
+
+
+// Async validator
 angular.module('angularMovieUI')
     .directive('titleValidator', titleValidatorDirective);
 
